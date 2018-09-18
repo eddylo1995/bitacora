4f0b008d36bbf57a780b03256d7d9d3e486f4ffc
eddylo1995 <41241710+eddylo1995@users.noreply.github.com>
2018-09-18 13:10:51 -0600

    se agrego el navegador

--- bitacora/bitacora/Form1.Designer.cs
+++ bitacora/bitacora/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace bitacora
 {
-    partial class Form1
+    partial class Bitacora
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.navegadorDiseño1 = new Navegador.navegadorDiseño();
             this.SuspendLayout();
             // 
-            // Form1
+            // navegadorDiseño1
+            // 
+            this.navegadorDiseño1.BackColor = System.Drawing.Color.Gainsboro;
+            this.navegadorDiseño1.Location = new System.Drawing.Point(1, -1);
+            this.navegadorDiseño1.Name = "navegadorDiseño1";
+            this.navegadorDiseño1.Size = new System.Drawing.Size(827, 63);
+            this.navegadorDiseño1.TabIndex = 0;
+            // 
+            // Bitacora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(153)))), ((int)(((byte)(183)))));
-            this.ClientSize = new System.Drawing.Size(821, 367);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(934, 661);
+            this.Controls.Add(this.navegadorDiseño1);
+            this.Name = "Bitacora";
+            this.Text = "Bitacora";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Navegador.navegadorDiseño navegadorDiseño1;
     }
 }
--- bitacora/bitacora/Form1.cs
+++ bitacora/bitacora/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace bitacora
 {
-    public partial class Form1 : Form
+    public partial class Bitacora : Form
     {
-        public Form1()
+        public Bitacora()
         {
             InitializeComponent();
         }
