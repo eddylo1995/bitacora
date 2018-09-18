c27f8953a7e47066394dc6e2cb7851c8c34d15b9
eddylo1995 <41241710+eddylo1995@users.noreply.github.com>
2018-09-18 12:39:02 -0600

    Add files via upload

---
+++ bitacora/bitacora/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitacora
+{
+    public class Class1
+    {
+    }
+}
