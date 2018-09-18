e6efd8a61d1b5b9b4dd89e27fd3f415a36e72b2e
eddylo1995 <41241710+eddylo1995@users.noreply.github.com>
2018-09-18 13:52:33 -0600

    a

--- bitacora/bitacora/Form1.Designer.cs
+++ bitacora/bitacora/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.navegador1 = new CapaDiseno.Navegador();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
@@ -102,6 +103,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.navegador1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Bitacora";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
